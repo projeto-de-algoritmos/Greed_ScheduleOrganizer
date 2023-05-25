a41e3f82d93b17e65a24d853afb03d527a872fba
Nicolas Souza <nszchagas@gmail.com>
2023-05-25 15:58:36 -0300

    Ajustando tela

--- main.js
+++ main.js
@@ -27,3 +27,7 @@ function getToday() {
 function initForm() {
     console.log('init')
 }
+
+function infoProcrastinador() {
+    alert('O modo procrastinador reduz a data de entrega da tarefa em um tempo aleatório para te ajudar a não procrastinar.')
+}
