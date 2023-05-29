77e427bc9600fe0d6539b2cbf25c7e2ff5fda0a9
Nicolas Souza <nszchagas@gmail.com>
2023-05-29 14:31:28 -0300

    wip: debugando algoritmo

--- tasklist.js
+++ tasklist.js
@@ -1,4 +1,3 @@
-const { duration } = require("moment");
 
 const table_ids =
 {
@@ -12,7 +11,6 @@ class TaskList extends Array {
     constructor() {
         super();
     }
-
 
     add(formValue) {
         let task = new Task(formValue)
@@ -44,6 +42,12 @@ class TaskList extends Array {
         ).join('')
         return table + tasksHTML + '</table>';
     }
+
+    swap(indexA, indexB) {
+        const temp = this[indexA];
+        this[indexA] = this[indexB]
+        this[indexB] = temp
+    }
 }
 
 // let tasks = [];
@@ -68,92 +72,73 @@ function renderTasks(table_type, deletable = true) {
     document.getElementById(table_ids[table_type].id).innerHTML = tasks.toTable(table_ids[table_type].headers, deletable)
 }
 
-function separate(tasks, left, right){
+function separate(left, right) {
 
-    let taskPivot = new Task();
-    taskPivot = tasks[right];
-
+    let taskPivot = tasks[right];
     let auxiliar = left;
 
-    for(let k = left; k < right; k++){
-        if(tasks[k].data_final <= taskPivot.data_final){
-            
+    for (let k = left; k < right; k++) {
+        if (tasks[k].data_final <= taskPivot.data_final) {
+
             //Troca de posição a task[k] com a task[auxiliar]
-            let taskToSave = new Task();
-            
-            taskToSave = tasks[auxiliar];
-            
+            let taskToSave = tasks[auxiliar];
             tasks[auxiliar] = tasks[k];
-
             tasks[k] = tasks[taskToSave];
-
             auxiliar++;
         }
     }
 
     //Troca de posição a task[auxiliar] com a task[right]
-    let taskToSave = new Task();
-
-    taskToSave = tasks[right];
-    
-    tasks[right] = tasks[auxiliar];
-
-    tasks[auxiliar] = tasks[right];
+    tasks.swap(right, auxiliar)
+    // let taskToSave = tasks[right];
+    // tasks[right] = tasks[auxiliar];
+    // tasks[auxiliar] = taskToSave;
 
     //Devolve a posição do Pivo
-    return auxiliar;    
+    return auxiliar;
 }
 
-function quickSort(tasks, left, right){
-    
-    let auxiliar;
+function quickSort(left, right) {
+    console.log(tasks)
+    const middle = Math.floor((left + right) / 2)
+    // tasks.reduce((v, t) => v += t?.nome + '\n')
 
-    if(right <= left){
+    if (right <= left) {
         return;
     }
 
+
+
     //Compara e troca
-    if(tasks[(left+right)/2].data_final < tasks[right].data_final){
-        
-        let taskToSave = new Task();
+    if (tasks[middle].data_final < tasks[right].data_final) {
+        tasks.swap(middle, right)
 
-        taskToSave = tasks[right];
-        
-        tasks[right] = tasks[(left+right)/2];
-
-        tasks[(left+right)/2] = tasks[right];
+        // let taskToSave = tasks[right];
+        // tasks[right] = tasks[middle];
+        // tasks[middle] = taskToSave;
     }
 
     //Compara e troca
-    if(tasks[left].data_final < tasks[(left+right)/2].data_final){
-    
-        let taskToSave = new Task();
-
-        taskToSave = tasks[left];
-        
-        tasks[left] = tasks[(left+right)/2];
-
-        tasks[(left+right)/2] = tasks[left];
+    if (tasks[left].data_final < tasks[middle].data_final) {
+        tasks.swap(left, middle)
+        // let taskToSave = tasks[left];
+        // tasks[left] = tasks[middle];
+        // tasks[middle] = taskToSave;
     }
 
     //Compara e troca
-    if(tasks[right].data_final < tasks[(left+right)/2].data_final){
-
-        let taskToSave = new Task();
-
-        taskToSave = tasks[right];
-        
-        tasks[right] = tasks[(left+right)/2];
-
-        tasks[(left+right)/2] = tasks[right];
+    if (tasks[right].data_final < tasks[middle].data_final) {
+        tasks.swap(right, middle)
+        // let taskToSave = tasks[right];
+        // tasks[right] = tasks[middle];
+        // tasks[middle] = taskToSave;
     }
 
     //Recebe a posição do Pivo
-    auxiliar =  separate(tasks);
-    
-    quickSort(tasks,left, (auxiliar - 1));
+    const auxiliar = separate(left, right);
 
-    quickSort(tasks, auxiliar+1, right);
+    quickSort(tasks, left, (auxiliar - 1));
+    quickSort(tasks, auxiliar + 1, right);
 
 }
 
@@ -161,19 +146,19 @@ function quickSort(tasks, left, right){
 function scheduleTasks() {
 
     // Ordenar o tasks 
-    quickSort(tasks, 0, (tasks.length -1))
+    quickSort(0, (tasks.length - 1))
 
     // Mostrar ordenadas - verificar se a tabela renderizou certinho
     renderTasks('ordenadas', false)
 
     // Agendar cada tarefa ordenada
-    
+
     tasks[0].data_de_inicio = 0;
     tasks[0].data_de_fim = tasks[0].data_de_inicio + tasks[0].duracao;
 
-    for(let i = 1; i < tasks.length; i ++){
-        
-        tasks[i].data_de_inicio = tasks[i-1].data_de_fim;
+    for (let i = 1; i < tasks.length; i++) {
+
+        tasks[i].data_de_inicio = tasks[i - 1].data_de_fim;
         tasks[i].data_de_fim = tasks[i].data_de_inicio + tasks[i].duracao;
     }
 
@@ -187,19 +172,18 @@ function init() {
     // @TODO: remover após os testes
 
 
-    tasks.add({ nome: "Tarefa 01", duracaoMinutos: 15, duracaoHoras: 2, duracaoDias: 0, dataFinal: "2023-05-29T05:02" })
-    tasks.add({ nome: "Tarefa 02", duracaoMinutos: 16, duracaoHoras: 3, duracaoDias: 1, dataFinal: "2023-05-29T21:44" })
-    tasks.add({ nome: "Tarefa 03", duracaoMinutos: 17, duracaoHoras: 4, duracaoDias: 2, dataFinal: "2023-05-29T00:10" })
+    tasks.add({ nome: "Tarefa 02", duracaoMinutos: 15, duracaoHoras: 2, duracaoDias: 0, dataFinal: "2023-05-29T05:02" })
     tasks.add({ nome: "Tarefa 04", duracaoMinutos: 18, duracaoHoras: 5, duracaoDias: 3, dataFinal: "2023-05-30T08:13" })
+    tasks.add({ nome: "Tarefa 01", duracaoMinutos: 17, duracaoHoras: 4, duracaoDias: 2, dataFinal: "2023-05-29T00:10" })
+    tasks.add({ nome: "Tarefa 03", duracaoMinutos: 16, duracaoHoras: 3, duracaoDias: 1, dataFinal: "2023-05-29T21:44" })
     tasks.add({ nome: "Tarefa 05", duracaoMinutos: 19, duracaoHoras: 6, duracaoDias: 4, dataFinal: "2023-06-05T20:50" })
-    tasks.add({ nome: "Tarefa 06", duracaoMinutos: 20, duracaoHoras: 7, duracaoDias: 5, dataFinal: "2023-06-01T18:06" })
-    tasks.add({ nome: "Tarefa 07", duracaoMinutos: 21, duracaoHoras: 8, duracaoDias: 6, dataFinal: "2023-06-04T02:22" })
-    tasks.add({ nome: "Tarefa 08", duracaoMinutos: 22, duracaoHoras: 9, duracaoDias: 0, dataFinal: "2023-06-04T15:10" })
-    tasks.add({ nome: "Tarefa 09", duracaoMinutos: 23, duracaoHoras: 10, duracaoDias: 1, dataFinal: "2023-06-06T03:23" })
-    tasks.add({ nome: "Tarefa 10", duracaoMinutos: 24, duracaoHoras: 11, duracaoDias: 2, dataFinal: "2023-06-03T10:51" })
-    tasks.add({ nome: "Tarefa 11", duracaoMinutos: 25, duracaoHoras: 12, duracaoDias: 3, dataFinal: "2023-06-06T20:14" })
-    tasks.add({ nome: "Tarefa 12", duracaoMinutos: 26, duracaoHoras: 13, duracaoDias: 4, dataFinal: "2023-06-01T05:56" })
-
+    // tasks.add({ nome: "Tarefa 06", duracaoMinutos: 20, duracaoHoras: 7, duracaoDias: 5, dataFinal: "2023-06-01T18:06" })
+    // tasks.add({ nome: "Tarefa 07", duracaoMinutos: 21, duracaoHoras: 8, duracaoDias: 6, dataFinal: "2023-06-04T02:22" })
+    // tasks.add({ nome: "Tarefa 08", duracaoMinutos: 22, duracaoHoras: 9, duracaoDias: 0, dataFinal: "2023-06-04T15:10" })
+    // tasks.add({ nome: "Tarefa 09", duracaoMinutos: 23, duracaoHoras: 10, duracaoDias: 1, dataFinal: "2023-06-06T03:23" })
+    // tasks.add({ nome: "Tarefa 10", duracaoMinutos: 24, duracaoHoras: 11, duracaoDias: 2, dataFinal: "2023-06-03T10:51" })
+    // tasks.add({ nome: "Tarefa 11", duracaoMinutos: 25, duracaoHoras: 12, duracaoDias: 3, dataFinal: "2023-06-06T20:14" })
+    // tasks.add({ nome: "Tarefa 12", duracaoMinutos: 26, duracaoHoras: 13, duracaoDias: 4, dataFinal: "2023-06-01T05:56" })
     scheduleTasks();
 
     return false;
